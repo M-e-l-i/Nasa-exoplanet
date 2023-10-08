c5b6281d71551d3c1b27e1bb0120151c46efdfc9
Abhiraman-S-Nair <84242314+Abhiraman-S-Nair@users.noreply.github.com>
2023-10-08 19:11:01 +0530

    Create index.jsx

---
+++ Web/Planet Overview/Desktop-3/index.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+    return (
+        <div className="desktop">
+            <div className="div">
+                <div className="text-wrapper">Astralis</div>
+                <div className="overlap">
+                    <div className="overlap-group">
+                        <div className="frame" />
+                        <p className="distance-from-earth">
+                            Distance from Earth: 420 light-years
+                            <br />
+                            <br />
+                            Diameter: 1.5 x Earth&#39;s diameter (~19,000 km)
+                            <br />
+                            <br />
+                            Mass: Corresponding to the diameter, could be more than 1.5 x Earth&#39;s due to denser composition
+                            <br />
+                            <br />
+                            Star Type: G-type main-sequence star (like our Sun)
+                            <br />
+                            <br />
+                            Orbital Period (Year Length):400 Earth days
+                            <br />
+                            <br />
+                            Composition: 78% Nitrogen, 21% Oxygen, 1% other gases
+                            <br />
+                            <br />
+                            Atmospheric Pressure:1.1 times Earth&#39;s
+                            <br />
+                            <br />
+                            Average Temperature: [e.g., 22Â°C]
+                            <br />
+                            <br />
+                            Prevailing Winds: Moderate, due to rotation and temperature differences
+                            <br />
+                            <br />
+                            Core: Active, molten iron-nickel core
+                            <br />
+                            <br />
+                            Tectonic Activity: High (similar to Earth)
+                            <br />
+                            <br />
+                            Major Landforms: [e.g., three supercontinents, multiple island chains]
+                        </p>
+                        <div className="text-wrapper-2">Description</div>
+                        <div className="rectangle" />
+                        <div className="text-wrapper-3">Know More</div>
+                    </div>
+                    <div className="overlap-group-2">
+                        <div className="rectangle-2" />
+                        <div className="text-wrapper-4">Back</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
